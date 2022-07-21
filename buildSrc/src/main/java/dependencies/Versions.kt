4b9ada946ffc746b2bdea83fead73d6b9a3c4df4
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2022-07-21 10:14:19 +0200

    Reorder versions lexicographically

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -6,13 +6,13 @@ object Versions {
     const val android_target_sdk = 31
 
     const val androidx_fragment = "1.5.0"
-    const val okhttp3 = "4.10.0"
+    const val coroutines = "1.6.4"
     const val dagger = "2.42"
+    const val espresso = "3.4.0"
+    const val glide = "4.13.2"
     const val kotlin = "1.7.10"
     const val mockito = "4.6.1"
+    const val okhttp3 = "4.10.0"
     const val robolectric = "4.8.1"
-    const val espresso = "3.4.0"
     const val work = "2.7.1"
-    const val coroutines = "1.6.4"
-    const val glide = "4.13.2"
 }
